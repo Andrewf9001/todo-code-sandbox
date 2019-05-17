979d57e4b2ed41676b80df001bfd64eb0d01ccd7
AndrewMPFQ <andrewf9001@gmail.com>
2019-05-17 14:42:14 -0600

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import TodoItem from "./todo-item";
 
@@ -15,6 +16,15 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch("http://localhost:5000/todos")
+      .then(response => response.json())
+      .then(data => this.setState({
+        todos: data
+      })
+    );
+  }
+
   onChange = event => {
     this.setState({
       todo: event.target.value
@@ -23,17 +33,49 @@ class App extends React.Component {
 
   renderTodos = () => {
     return this.state.todos.map(item => {
-      return <TodoItem title={item} />;
+      return (
+        <TodoItem 
+          key={item[0]}
+          id={item[0]}
+          title={item[1]}
+          done={item[2]}
+          deleteItem={this.deleteItem} 
+        />
+      );
     });
   };
 
   addTodo = event => {
     event.preventDefault();
-    this.setState({
-      todos: [...this.state.todos, this.state.todo],
-      todo: ""
-    });
+    axios({
+      method: "post",
+      url: "http://localhost:5000/add-todo",
+      headers: { "content-type": "application/json" },
+      data: {
+        title: this.state.todo,
+        done: false
+      }
+    })
+    .then(data => {
+      this.setState({
+        todos: [...this.state.todos, data.data],
+        todo: ""
+      })
+    })
+    .catch(error => console.log(error));
   };
+
+  deleteItem = id => {
+    fetch(`http://localhost:5000/todo/${id}`, {
+      method: "DELETE"
+    })
+    .then(this.setState({
+      todos: this.state.todos.filter(item => {
+        return item[0] !== id
+      })
+    }))
+    .catch(error => console.log(error));
+  }
 
   render() {
     return (
@@ -42,7 +84,7 @@ class App extends React.Component {
         <form className="add-todo" onSubmit={this.addTodo}>
           <input
             type="text"
-            placeholder="Add ToDo"
+            placeholder=" Add ToDo"
             onChange={this.onChange}
             value={this.state.todo}
           />
--- src/todo-item.js
+++ src/todo-item.js
@@ -5,18 +5,36 @@ class TodoItem extends React.Component {
     super(props);
 
     this.state = {
-      done: false
+      done: props.done
     };
   }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: this.props.title,
+        done: !this.state.done
+      })
+    })
+      .then(this.setState({
+        done: !this.state.done
+      })
+    )
+    .catch(error => console.log(error));
+  };
 
   render() {
     return (
       <div className="todo-item">
         <input
           type="checkbox"
-          onClick={() => this.setState({ done: !this.state.done })}
+          onClick = {this.toggleDone}
+          defaultChecked={this.state.done}
         />
         <p className={this.state.done ? "done" : null}>{this.props.title}</p>
+        <button onClick={() => this.props.deleteItem(this.props.id)}>X</button>
       </div>
     );
   }
